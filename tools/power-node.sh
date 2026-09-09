9676ce715a4432096b96115605cce74818e243bf
Ben McDonald <ben.mcdonald@hpe.com>
2026-09-09 14:29:17 -0700

    add initial files
    Signed-off-by: Ben McDonald <ben.mcdonald@hpe.com>

---
+++ tools/power-node.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -eu
+
+# 1. Read injected inputs
+node_id="${WARP_INPUT_NODE_ID}"
+action="${WARP_INPUT_ACTION}"
+
+# 2. Execute magellan CLI using the inputs
+# (Assuming magellan outputs a JSON status string)
+magellan_output=$(magellan power set --node "${node_id}" --action "${action}")
+
+# 3. Write the required 'power_status' output to the WARP_OUTPUT path
+if [ -n "${WARP_OUTPUT:-}" ]; then
+  echo "power_status=${magellan_output}" >> "${WARP_OUTPUT}"
+fi
